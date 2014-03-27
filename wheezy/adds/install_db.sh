3f08601d3fe918c103b309b24d1414cb9d7617bd
Thomas Thune Hansen <tth@thomastnsensmbp.mydomain>
2014-03-27 21:34:52 +0100

    updated configuration stuff

--- wheezy/adds/install_db.sh
+++ wheezy/adds/install_db.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-mysql_install_db
-
-mysqld_safe &
-sleep 5
 touch /var/www/logs/php.log
 touch /var/www/logs/error.log
 touch /var/www/logs/access.log
 chown -R www-data: /var/www
 chmod g+rw -R /var/www
 
-echo "Create db"
-mysql -e "CREATE DATABASE drupal; GRANT ALL ON drupal.* TO 'drupal'@'localhost' IDENTIFIED BY 'drupal';"
+echo "==> Installing db"
+mysql_install_db > /dev/null
 
-echo "Set root password"
-mysqladmin -u root password 'database'
+echo ""
+echo "==> Starting mysqld"
+cd /usr ; /usr/bin/mysqld_safe &
+sleep 5
+
+echo ""
+echo "==> Create db"
+mysql -e "CREATE DATABASE db; GRANT ALL ON db.* TO 'db'@'localhost' IDENTIFIED BY 'db';"
+
+echo ""
+echo "Initialized site. Created db:"
+echo "  database: db"
+echo "  user: db"
+echo "  pass: db"
+echo ""
+echo "...Done!"
